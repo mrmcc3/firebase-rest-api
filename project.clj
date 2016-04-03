b77ad81cb441943cd7a6460578bdba1884bffee8
Michael McClintock <mrmcc3@users.noreply.github.com>
2016-04-03 12:19:13 +1000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mrmcc3/firebase-rest-clj "0.1.0-SNAPSHOT"
   :description "Use the Firebase REST API from clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mrmcc3/firebase-rest-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
